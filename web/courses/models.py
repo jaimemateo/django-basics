fb1e20d42a34723dd102bfb71a4505f089bcd23d
Jaime <jaime.mateoh@gmail.com>
2016-04-29 20:14:23 +0200

    Modified description to CharField

--- web/courses/models.py
+++ web/courses/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Course(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
