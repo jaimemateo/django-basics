30c9b35afab281c9adb7adb2adb6ef53221b84eb
Jaime <jaime.mateoh@gmail.com>
2016-04-29 19:59:23 +0200

    Create new view for courses

--- web/courses/models.py
+++ web/courses/models.py
@@ -6,4 +6,7 @@ from django.db import models
 class Course(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
-    descripton = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
