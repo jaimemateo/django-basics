820b972b07a4bebde80ada7548e693376488baa7
Jaime <jaime.mateoh@gmail.com>
2016-05-12 20:11:01 +0200

    Add A Detail View

--- web/courses/views.py
+++ web/courses/views.py
@@ -3,8 +3,13 @@ from django.http import HttpResponse
 
 from .models import Course
 
+
 # Create your views here.
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'courses/course_list.html', {'courses': courses})
 
+
+def course_detail(request, pk):
+    course = Course.objects.get(pk=pk)
+    return render(request, 'courses/course_detail.html', {'course': course})
